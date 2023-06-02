5338d6f8d4c8e529ad839033bec49109bd7f1733
Mostafa Elsayed <mostafa@haleytek.com>
2023-06-02 19:42:42 +0200

    Added fix for chrome driver

--- Demo/TestCase01.py
+++ Demo/TestCase01.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-from pyvirtualdisplay import Display
 
 def execute_test_case(useremail, userpassword):
-    print("test case 01 started")
-    display = Display(visible=0, size=(800, 600))
-    display.start()
+    print("test case 01 started")>
     driver = webdriver.Firefox()
     #maximize the window size
     driver.maximize_window()
